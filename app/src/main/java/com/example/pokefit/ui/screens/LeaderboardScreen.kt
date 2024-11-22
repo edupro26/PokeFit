43cabc388341b42a058feed91270cab0c226a0c0
edupro26 <edupro26@gmail.com>
2024-11-22 21:39:37 +0000

    Created dummy main screens

---
+++ app/src/main/java/com/example/pokefit/ui/screens/LeaderboardScreen.kt
@@ -0,0 +1,43 @@
+package com.example.pokefit.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LeaderboardScreen() {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Leaderboard Screen",
+            fontSize = MaterialTheme.typography.titleLarge.fontSize,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = "This will be the screen where you can see the leaderboards of the app.",
+            fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+            color = Color.Black
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun LeaderboardScreenPreview() {
+    LeaderboardScreen()
+}
