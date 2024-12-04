e453d180dc940aae0219ed083087814b2de6d4e1
edupro26 <edupro26@gmail.com>
2024-12-04 23:18:22 +0000

    Fixed merge conflict between dev and main

--- app/src/androidTest/java/pt/ul/fc/cm/pokefit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/pt/ul/fc/cm/pokefit/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("pt.ul.fc.cm.pokefit.R", appContext.packageName)
+        assertEquals("pt.ul.fc.cm.pokefit", appContext.packageName)
     }
 }
