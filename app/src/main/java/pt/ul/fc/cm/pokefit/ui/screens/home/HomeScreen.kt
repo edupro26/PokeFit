f1fc5242b8e3ac0db0f6844a30d353284f5e8a79
edupro26 <edupro26@gmail.com>
2024-12-10 20:10:56 +0000

    Created components packages for all screens

--- app/src/main/java/pt/ul/fc/cm/pokefit/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/pt/ul/fc/cm/pokefit/ui/screens/home/HomeScreen.kt
@@ -8,25 +8,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -42,12 +34,10 @@ import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import pt.ul.fc.cm.pokefit.R
-import pt.ul.fc.cm.pokefit.ui.theme.Pink80
-import pt.ul.fc.cm.pokefit.ui.theme.Primary
+import pt.ul.fc.cm.pokefit.ui.screens.home.components.ScreenTopBar
+import pt.ul.fc.cm.pokefit.ui.screens.home.components.StatsSection
 import pt.ul.fc.cm.pokefit.ui.theme.PrimaryGrey
 import pt.ul.fc.cm.pokefit.ui.theme.Transparent
 
@@ -63,46 +53,7 @@ fun HomeScreen(viewModel: HomeViewModel = androidx.lifecycle.viewmodel.compose.v
         modifier = Modifier
             .fillMaxSize()
             .nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            TopAppBar(
-                title = {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Image(
-                            modifier = Modifier.size(38.dp),
-                            painter = painterResource(id = R.drawable.ic_app_logo),
-                            contentDescription = "App Logo"
-                        )
-                        Text(
-                            text = buildAnnotatedString {
-                                withStyle(style = SpanStyle(color = Color.Black)) {
-                                    append("POKÉ")
-                                }
-                                withStyle(style = SpanStyle(color = Primary)) {
-                                    append("FIT")
-                                }
-                            },
-                            fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                            fontWeight = FontWeight.SemiBold
-                        )
-                    }
-                },
-                actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.Notifications,
-                            contentDescription = "Notifications"
-                        )
-                    }
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            imageVector = Icons.Default.DateRange,
-                            contentDescription = "Goals"
-                        )
-                    }
-                },
-                scrollBehavior = scrollBehavior
-            )
-        }
+        topBar = { ScreenTopBar(scrollBehavior) }
     ) { paddingValues ->
         Column(
             modifier = Modifier
@@ -116,30 +67,7 @@ fun HomeScreen(viewModel: HomeViewModel = androidx.lifecycle.viewmodel.compose.v
                 pokemonImage = painterResource(id = R.drawable.trainer),
                 steps = steps,
             )
-
             Spacer(modifier = Modifier.height(24.dp))
-
-//            // General Stats
-//            Text(
-//                text = "Steps Taken: $steps",
-//                style = MaterialTheme.typography.headlineMedium,
-//                color = Color.Black
-//            )
-//            Spacer(modifier = Modifier.height(16.dp))
-//            LinearProgressIndicator(
-//                progress = { progress },
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .height(8.dp)
-//                    .padding(horizontal = 16.dp),
-//                color = Primary,
-//            )
-//            Spacer(modifier = Modifier.height(8.dp))
-//            Text(
-//                text = "Goal: $stepGoal steps",
-//                style = MaterialTheme.typography.labelLarge,
-//                color = Color.Gray
-//            )
         }
     }
 }
@@ -288,102 +216,4 @@ fun PokemonStatsSection(
             }
         }
     }
-}
-
-
-@Composable
-fun StatsSection(steps: String, heartRate: String, sleepDuration: String) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(188.dp)
-            .padding(horizontal = 20.dp),
-        horizontalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        // Coluna contendo Steps e Sleep
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight(),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-            StatItem(
-                label = "Steps",
-                value = steps,
-                indicatorColor = Color(0xFF4CAF50) // Green
-            )
-            StatItem(
-                label = "Sleep",
-                value = sleepDuration,
-                indicatorColor = Color(0xFFE91E63) // Pink
-            )
-        }
-
-        // Cartão de Heart Rate
-        Box(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .clip(RoundedCornerShape(12.dp)) // Cantos arredondados
-                .background(MaterialTheme.colorScheme.surface) // Cor de fundo
-        ) {
-                StatItem(
-                    label = "Heart Rate",
-                    value = heartRate,
-                    indicatorColor = Color(0xFFFF9800), // Laranja
-                    size = 172.dp // Define altura para o StatItem
-                )
-
-        }
-    }
-}
-
-@Composable
-fun StatItem(label: String, value: String, indicatorColor: Color, size: Dp = 80.dp) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(size)
-            .clip(RoundedCornerShape(12.dp))
-            .background(PrimaryGrey)
-            .padding(vertical = 8.dp)
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            // Label
-            Text(
-                text = label,
-                style = MaterialTheme.typography.bodySmall,
-                color = MaterialTheme.colorScheme.onSurface,
-                fontWeight = FontWeight.Medium
-            )
-            // Value
-            Text(
-                text = value,
-                style = MaterialTheme.typography.headlineMedium,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.onSurface
-            )
-            // Indicator
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(6.dp)
-                    .clip(RoundedCornerShape(bottomStart = 12.dp, bottomEnd = 12.dp))
-                    .background(indicatorColor)
-            )
-        }
-    }
-}
-
-
-@Composable
-@Preview(showBackground = true)
-fun HomeScreenPreview() {
-    HomeScreen()
 }
---
+++ app/src/main/java/pt/ul/fc/cm/pokefit/ui/screens/pokemon/components/PokemonCard.kt
@@ -0,0 +1,64 @@
+package pt.ul.fc.cm.pokefit.ui.screens.pokemon.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import pt.ul.fc.cm.pokefit.domain.Pokemon
+
+@Composable
+fun PokemonCard(pokemon: Pokemon) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .size(100.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            // Pokémon Image
+            Image(
+                painter = rememberAsyncImagePainter(model = pokemon.imageUrl),
+                contentDescription = "${pokemon.name} image",
+                modifier = Modifier.size(94.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            // Pokémon Details
+            Column {
+                Text(
+                    text = pokemon.name.replaceFirstChar { it.uppercase() },
+                    fontWeight = FontWeight.Bold,
+                    fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                    color = Color.Black
+                )
+                Text(
+                    text = "Types: ${pokemon.types.joinToString(", ") {
+                        it.replaceFirstChar { it.uppercase() }
+                    }}",
+                    fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                    color = Color.Gray
+                )
+            }
+        }
+    }
+}
