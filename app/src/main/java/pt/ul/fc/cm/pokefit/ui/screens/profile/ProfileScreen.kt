465ede3bea4c367b56bff081614587880f21b4e3
Eduardo Proença <edupro26@gmail.com>
2024-12-15 20:16:36 +0000

    Removed unused import directives

--- app/src/main/java/pt/ul/fc/cm/pokefit/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/pt/ul/fc/cm/pokefit/ui/screens/profile/ProfileScreen.kt
@@ -1,19 +1,13 @@
 package pt.ul.fc.cm.pokefit.ui.screens.profile
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,22 +15,16 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
 import pt.ul.fc.cm.pokefit.ui.common.BottomAppBar
 import pt.ul.fc.cm.pokefit.ui.screens.profile.components.ScreenTopBar
-import pt.ul.fc.cm.pokefit.R
-import pt.ul.fc.cm.pokefit.domain.model.User
 import pt.ul.fc.cm.pokefit.ui.screens.profile.components.DisplayName
 import pt.ul.fc.cm.pokefit.ui.screens.profile.components.ProfilePicture
-import pt.ul.fc.cm.pokefit.ui.theme.PrimaryGrey
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
