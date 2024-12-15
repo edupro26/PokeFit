fa72ed1935e4f114bc9a64db81dc8df1f4be36b1
edupro26 <edupro26@gmail.com>
2024-12-15 03:21:37 +0000

    Updated navigation

--- app/src/main/java/pt/ul/fc/cm/pokefit/ui/navigation/NavGraph.kt
+++ app/src/main/java/pt/ul/fc/cm/pokefit/ui/navigation/NavGraph.kt
@@ -4,8 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import pt.ul.fc.cm.pokefit.ui.screens.auth.LoginScreen
-import pt.ul.fc.cm.pokefit.ui.screens.auth.SignupScreen
+import pt.ul.fc.cm.pokefit.ui.screens.auth.signin.SigninScreen
+import pt.ul.fc.cm.pokefit.ui.screens.auth.signup.SignupScreen
+import pt.ul.fc.cm.pokefit.ui.screens.auth.splash.SplashScreen
 import pt.ul.fc.cm.pokefit.ui.screens.home.HomeScreen
 import pt.ul.fc.cm.pokefit.ui.screens.leaderboard.LeaderboardScreen
 import pt.ul.fc.cm.pokefit.ui.screens.pokemon.PokemonScreen
@@ -15,10 +16,13 @@ import pt.ul.fc.cm.pokefit.ui.screens.profile.ProfileScreen
 fun NavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Screen.Login.route
+        startDestination = Screen.Splash.route
     ) {
-        composable(route = Screen.Login.route) {
-            LoginScreen(navController)
+        composable(route = Screen.Splash.route) {
+            SplashScreen(navController)
+        }
+        composable(route = Screen.Signin.route) {
+            SigninScreen(navController)
         }
         composable(route = Screen.Signup.route) {
             SignupScreen(navController)
