418566c14d2c66b1c9e30a1eb18540d2066f9af9
edupro26 <edupro26@gmail.com>
2024-12-30 23:54:54 +0000

    Added a surface in InitialScreen

--- app/src/main/java/pt/ul/fc/cm/pokefit/presentation/screens/initial/InitialScreen.kt
+++ app/src/main/java/pt/ul/fc/cm/pokefit/presentation/screens/initial/InitialScreen.kt
@@ -3,6 +3,7 @@ package pt.ul.fc.cm.pokefit.presentation.screens.initial
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,21 +15,25 @@ fun InitialScreen(
     navigate: (String, Boolean) -> Unit,
     viewModel: InitialViewModel = hiltViewModel(),
 ) {
-    val authState = viewModel.authState.value
-    if (authState.isLoading) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator()
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val authState = viewModel.authState.value
+        if (authState.isLoading) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        } else if (!authState.isSignedIn) {
+            /**
+             * TODO small app description and greetings
+             * TODO continue to signin ou signout
+             */
+            navigate(Screen.Signin.route, true)
+        } else {
+            navigate(Screen.Home.route, true)
         }
-    } else if (!authState.isSignedIn) {
-        /**
-         * TODO small app description and greetings
-         * TODO continue to signin ou signout
-         */
-        navigate(Screen.Signin.route, true)
-    } else {
-        navigate(Screen.Home.route, true)
     }
 }
