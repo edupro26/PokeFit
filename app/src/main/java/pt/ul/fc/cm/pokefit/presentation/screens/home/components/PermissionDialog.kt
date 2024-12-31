06e9657b070136686ab8900b1b64d37394fd518d
edupro26 <edupro26@gmail.com>
2024-12-31 02:07:09 +0000

    Created new permission request logic

---
+++ app/src/main/java/pt/ul/fc/cm/pokefit/presentation/screens/home/components/PermissionDialog.kt
@@ -0,0 +1,46 @@
+package pt.ul.fc.cm.pokefit.presentation.screens.home.components
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PermissionDialog(
+    title: String,
+    contentText: String,
+    buttonText: String,
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit = {}
+) {
+    val context = LocalContext.current
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        title = { Text(title) },
+        text = { Text(contentText) },
+        confirmButton = {
+            TextButton(onClick = {
+                onConfirm()
+                openSettings(context)
+            }) {
+                Text(buttonText)
+            }
+        },
+        containerColor = MaterialTheme.colorScheme.surfaceContainer,
+        tonalElevation = 8.dp
+    )
+}
+
+private fun openSettings(context: Context) {
+    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+        data = Uri.fromParts("package", context.packageName, null)
+    }
+    context.startActivity(intent)
+}
